78882a1f74543a88cc15721f5cae85c2ef7035ca
denikeev <script.edf@gmail.com>
2022-12-08 21:09:55 +0300

    import React to jsx

--- frontend/src/slices/index.js
+++ frontend/src/slices/index.js
@@ -3,13 +3,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import authSlice from './authSlice.js';
 import channelsSlice from './channelsSlice.js';
 import messagesSlice from './messagesSlice.js';
-import modalSlice from './modalSlice.js';
 
 export default configureStore({
   reducer: {
     auth: authSlice,
     channels: channelsSlice,
     messages: messagesSlice,
-    modal: modalSlice,
   },
 });
